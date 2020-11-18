cfce7e94c713411a42ac12548e0ffbef3f3b99a4
Adi Cicurel <adici@wix.com>
2020-11-18 11:58:35 +0200

    Adding empty state theme

--- src/Themes/businessDashboard/docs/components/EmptyState.js
+++ src/Themes/businessDashboard/docs/components/EmptyState.js
@@ -1,48 +1,40 @@
-import {
-  alignCenter,
-  alignEnd,
-  alignStart,
-  children,
-  pageNoBorderTheme,
-  pageTheme,
-  structure,
-  sectionTheme,
-} from '../../../../EmptyState/docs/examples';
-
 export const playground = `
 <div>
   <ThemeProvider theme={theme()}>
+  <Box padding="10px" align="center">
+    <EmptyState
+      theme={'page-no-border'}
+      image={<div style={{ height: 120, width: 120, backgroundColor: '#dfe5eb', borderRadius: '50%', lineHeight: '120px' }}>120x120</div>}
+      title="You don't have any items yet"
+      subtitle='Create your product item in an easy & fast way to display it on your site'
+    />
+  </Box>
+  <Divider/>
     <Box padding="10px" align="center">
-      ${structure}
-    </Box>
-    <Divider/>
-    <Box padding="10px" align="center">
-      ${pageTheme}
-    </Box>
-    <Divider/>
-    <Box padding="10px" align="center">
-      ${pageNoBorderTheme}
-    </Box>
-    <Divider/>
-    <Box padding="10px" align="center">
-      ${sectionTheme}
-    </Box>
-    <Divider/>
-    <Box padding="10px" align="center">
-      ${children}
-    </Box>
-    <Divider/>
-    <Box padding="10px" align="center">
-      ${alignStart}
-    </Box>
-    <Divider/>
-    <Box padding="10px" align="center">
-      ${alignCenter}
-    </Box>
-    <Divider/>
-    <Box padding="10px" align="center">
-      ${alignEnd}
-    </Box>
+    <EmptyState
+      theme={'page-no-border'}
+      image={<div style={{ height: 60, width: 60, backgroundColor: '#dfe5eb', borderRadius: '50%', lineHeight: '60px' }}>60x60</div>}
+      title="You don't have any items yet"
+      subtitle='Create your product item in an easy & fast way to display it on your site'
+    />
+  </Box>
+  <Divider/>
+  <Box padding="10px" align="center">
+    <EmptyState
+      theme={'section'}
+      title="Section Theme"
+      subtitle='Create your product item in an easy & fast way to display it on your site' />
+  </Box>
+  <Divider/>
+  <Box padding="10px" align="center">
+    <EmptyState
+      theme={'page-no-border'}
+      title="You don't have any items yet"
+    >
+      {<Button priority="secondary" prefixIcon={<Icons.Add />}>New Item</Button>}
+    </EmptyState>
+
+  </Box>
   </ThemeProvider>
 </div>
 `;
